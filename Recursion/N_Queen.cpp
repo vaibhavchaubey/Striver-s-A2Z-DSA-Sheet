aa3426e402dbad013c9afeefc56ac3335fd8626f
Vaibhav <vaibhavchaubey786@gmail.com>
2024-06-04 00:10:01 +0530

    Recursion questions solved

--- Recursion/N_Queen.cpp
+++ Recursion/N_Queen.cpp
@@ -13,7 +13,7 @@ both indicate a queen and an empty space, respectively. */
 /* Time Complexity: (N! * N) 
 Exponential in nature, since we are trying out all ways.
 
-Space Complexity: O(N^2)                */
+Space Complexity: O(N^2)    */
 
 
 // class Solution {
@@ -80,6 +80,62 @@ Space Complexity: O(N^2)                */
 //         vector<string> board(n, string(n, '.'));
 
 //         solve(0, board, ans, n);
+
+//         return ans;
+//     }
+// };
+
+
+
+
+/* Solution 2: Backtracking
+Intuition: This is the optimization of the issafe function. In the previous issafe function,
+we need o(N) for a row, o(N) for the column, and o(N) for the diagonal. Here, we will use
+hashing to maintain a list to check whether that position can be the right one or not. */
+
+/* Time Complexity: (N! * N) 
+Exponential in nature, since we are trying out all ways.
+
+Space Complexity: O(N^2)    */
+
+
+// class Solution {
+// public:
+
+//     void solve(int col, vector<string>& board, vector<vector<string>>& ans, vector<int>& leftRow, vector<int>& lowerDiagonal, vector<int>& upperDiagonal, int n) {
+//         if (col == n) {
+//             ans.push_back(board);
+//             return;
+//         }
+
+//         for (int row = 0; row < n; row++) {
+//             if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[n - 1 + col - row] == 0){
+//                 board[row][col] = 'Q';
+//                 leftRow[row] = 1;
+//                 lowerDiagonal[row + col] = 1;
+//                 upperDiagonal[n - 1 + col - row] = 1;
+//                 solve(col + 1, board, ans, leftRow, lowerDiagonal, upperDiagonal, n);
+//                 // backtracking
+//                 board[row][col] = '.';
+//                 leftRow[row] = 0;
+//                 lowerDiagonal[row + col] = 0;
+//                 upperDiagonal[n - 1 + col - row] = 0;
+//             }
+//         }
+//     }
+//     vector<vector<string>> solveNQueens(int n) {
+//         vector<vector<string>> ans;
+
+//         // vector<string> board(n);
+//         // string s(n, '.');
+//         // for (int i = 0; i < n; i++) {
+//         //     board[i] = s;
+//         // }
+
+//         vector<string> board(n, string(n, '.'));
+//         vector<int> leftRow(n, 0), lowerDiagonal(2 * n - 1, 0), upperDiagonal(2 * n - 1, 0); 
+        
+//         solve(0, board, ans, leftRow, lowerDiagonal, upperDiagonal, n);
 
 //         return ans;
 //     }
