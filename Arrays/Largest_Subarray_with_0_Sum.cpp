3b21c313fbc39941194406c5bcb30e23bec19eb6
Vaibhav <vaibhavchaubey786@gmail.com>
2025-04-01 03:12:14 +0530

    Topic: Array

--- Arrays/Largest_Subarray_with_0_Sum.cpp
+++ Arrays/Largest_Subarray_with_0_Sum.cpp
@@ -1,18 +1,16 @@
 /* GFG  Submission    (Largest subarray with 0 sum) */
 
-/* Problem Statement: Given an array having both positive and negative integers. The task is to compute the 
+/* Problem Statement: Given an array having both positive and negative integers. The task is to compute the
 length of the largest subarray with sum 0. */
 
-
 /* Solution: Using Hashing */
-// Time Complexity: O(NlogN)
+// Time Complexity: O(N)
 // Space Complexity: O(N)
-
 
 // class Solution{
 //     public:
 //     int maxLen(vector<int>&A, int n)
-//     {   
+//     {
 //         int sum = 0;
 //         int maxLength = 0;
 //         unordered_map<int, int> mp;        // sum, index
@@ -29,7 +27,7 @@ length of the largest subarray with sum 0. */
 //                 mp[sum] = i;
 //             }
 //         }
-        
+
 //         return maxLength;
 //     }
 // };
