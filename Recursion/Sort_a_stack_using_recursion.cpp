ab763ab0cf66f501d274956b5e09fbeaf224ab10
Vaibhav <vaibhavchaubey786@gmail.com>
2025-05-12 01:06:24 +0530

    Topic: Recursion

--- Recursion/Sort_a_stack_using_recursion.cpp
+++ Recursion/Sort_a_stack_using_recursion.cpp
@@ -5,27 +5,25 @@ Note:
 Looping through the stack is not allowed.
 You need to return a stack that is sorted in descending order.
 For example:
-Given stack S = 1 3 2 
+Given stack S = 1 3 2
 The output will be 3 2 1 since it is the sorted order. */
 
-
 /* Solution:  */
-/* Approach: The main idea is to remove the elements recursively from the stack until the stack becomes empty and then insert 
-those values (from the call-stack  [A call-stack is the stack that is made in the memory when recursive-calls are made. ]), 
+/* Approach: The main idea is to remove the elements recursively from the stack until the stack becomes empty and then insert
+those values (from the call-stack  [A call-stack is the stack that is made in the memory when recursive-calls are made. ]),
 back into the stack in a sorted position.
 
     In each call, remove the top element from the stack.
     Make a recursive call to the function sortStack.
     After the call, insert the element in the stack in a sorted fashion.
-    This can be done by removing the top element from the stack until the topmost element is greater than the current element 
-    that we want to insert. 
+    This can be done by removing the top element from the stack until the topmost element is greater than the current element
+    that we want to insert.
 */
 
 // Time Complexity: O(N * N)
 // Space Complexity: O(N)                   // recursion stack
 
-
-// #include <bits/stdc++.h> 
+// #include <bits/stdc++.h>
 // void sortedInsert(stack<int> &s, int key){
 // 	if(s.empty() || key > s.top()){
 // 		s.push(key);
@@ -48,7 +46,7 @@ back into the stack in a sorted position.
 
 // 	int top = s.top();
 // 	s.pop();
-	
+
 // 	// Recur for the remaining elements in the stack.
 // 	sortStackHelper(s);
 
