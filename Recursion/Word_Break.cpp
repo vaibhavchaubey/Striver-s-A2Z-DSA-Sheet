09a9c91ca9702e95d5cc7a5e9f16c7d449876067
Vaibhav <vaibhavchaubey786@gmail.com>
2025-05-24 00:22:28 +0530

    Topic: Recursion

--- Recursion/Word_Break.cpp
+++ Recursion/Word_Break.cpp
@@ -1,19 +1,16 @@
 /* Leetcode  Submission   (139. Word Break)  */
 
-
-/* Problem Statement: Given a string s and a dictionary of strings wordDict, return true if s can be 
+/* Problem Statement: Given a string s and a dictionary of strings wordDict, return true if s can be
 segmented into a space-separated sequence of one or more dictionary words.
 Note that the same word in the dictionary may be reused multiple times in the segmentation. */
 
-
 /* Solution 1: Backtracking + Hash Table   (It will give TLE)  */
 /* Time Complexity : O(2^N)
-Given a string of length N, there are N+1 ways to split it into two parts. At eachstep, we have a choice: 
+Given a string of length N, there are N+1 ways to split it into two parts. At eachstep, we have a choice:
 to split or not to split. In the worse case, when all choices are to be checked, that results in O(2^N).
 
 Space Complexity : O(N)
 The depth of the recursion tree can go upto N. */
-
 
 // class Solution {
 // public:
@@ -37,16 +34,13 @@ The depth of the recursion tree can go upto N. */
 //     }
 // };
 
-
-
 /* Solution 2: Backtracking + Hash Table   (It will give TLE)  */
 /* Time Complexity : O(2^N)
-Given a string of length N, there are N+1 ways to split it into two parts. At eachstep, we have a choice: 
+Given a string of length N, there are N+1 ways to split it into two parts. At eachstep, we have a choice:
 to split or not to split. In the worse case, when all choices are to be checked, that results in O(2^N).
 
 Space Complexity : O(N)
 The depth of the recursion tree can go upto N. */
-
 
 // class Solution {
 // public:
@@ -70,18 +64,15 @@ The depth of the recursion tree can go upto N. */
 //     }
 // };
 
-
-
 /* Solution 3: DP(Memoisation) + Hash Table */
 /* Time Complexity : O(N*N)
-Each subproblem, defined by the starting index ind, is solved at most once due to memoization. 
-For each starting index ind, the helper function checks all possible end indices from ind+1 to s.size(). 
+Each subproblem, defined by the starting index ind, is solved at most once due to memoization.
+For each starting index ind, the helper function checks all possible end indices from ind+1 to s.size().
 Thus, in the worst case, it checks all substrings of the given string s.
 
-Space Complexity : O(N) + O(N) + O(M)              
-Memoization Array, Recursion Stack, Hash Set(O(M))     
+Space Complexity : O(N) + O(N) + O(M)
+Memoization Array, Recursion Stack, Hash Set(O(M))
 where m is the total number of characters in all words in wordDict. */
-
 
 // class Solution {
 // public:
@@ -112,51 +103,30 @@ where m is the total number of characters in all words in wordDict. */
 //     }
 // };
 
-
-
 /* Solution 4: DP(Tabulation) + Hash Table */
 /* Time Complexity : O(N*N)
 
-Space Complexity : O(N) + O(M)              
-Memoization Array, Hash Set(O(M))     
+Space Complexity : O(N) + O(M)
+Memoization Array, Hash Set(O(M))
 where m is the total number of characters in all words in wordDict. */
-
 
 // class Solution {
 // public:
-//     bool helper(int ind, string s, unordered_set<string>& set, vector<int>& dp){
-//         if(ind == s.size()){
-//             return true;
-//         }
-
-//         if(dp[ind] != -1){
-//             return dp[ind];
-//         }
-
-//         for(int i = ind; i < s.size(); i++){
-//             if(set.count(s.substr(ind, i+1-ind)) && helper(i+1, s, set, dp)){
-//                 return dp[ind] = true;
-//             }
-//         }
-
-//         return dp[ind] = false;
-//     }
 //     bool wordBreak(string s, vector<string>& wordDict) {
 //         int n = s.size();
 //         unordered_set<string> set(wordDict.begin(), wordDict.end());
 
 //         vector<bool> dp(n+1, false);
-//         dp[0] = true;
+//         dp[n] = true;
 
-//         for(int i = 1; i <= n; i++){
-//             for(int j = 0; j < i; j++){
-//                 if(dp[j] && set.count(s.substr(j, i-j))){
+//         for(int i = n-1; i >= 0; i--){
+//             for(int j = i; j < n; j++){
+//                 if(set.count(s.substr(i, j+1-i)) && dp[j + 1]){
 //                     dp[i] = true;
-//                     break;
 //                 }
 //             }
 //         }
 
-//         return dp[n];
+//         return dp[0];
 //     }
 // };
