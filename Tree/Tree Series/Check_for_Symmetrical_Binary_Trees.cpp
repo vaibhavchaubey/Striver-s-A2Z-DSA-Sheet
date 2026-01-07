39a84aabbca0fa905f7f5f7f110ff30f7bfe784e
Vaibhav <vaibhavchaubey786@gmail.com>
2026-01-08 02:28:15 +0530

    Topic: Trees

--- Tree/Tree Series/Check_for_Symmetrical_Binary_Trees.cpp
+++ Tree/Tree Series/Check_for_Symmetrical_Binary_Trees.cpp
@@ -26,3 +26,32 @@
 //         return isSymmetricHelp(root->left,root->right);
 //     }
 // };
+
+
+/* OR */
+
+// class Solution {
+// public:
+//     bool isSymmetricHelp(TreeNode* root1, TreeNode* root2){
+//         if(root1 == NULL && root2 == NULL){
+//             return true;
+//         }
+//         else if(root1 == NULL || root2 == NULL){
+//             return false;
+//         }
+//         else{
+//             bool cond1 = root1->val == root2->val;
+//             bool cond2 = isSymmetricHelp(root1->left,root2->right);
+//             bool cond3 = isSymmetricHelp(root1->right,root2->left);
+
+//             return cond1 && cond2 && cond3;
+//         }
+//     }
+//     bool isSymmetric(TreeNode* root) {
+//         if(root == NULL){
+//             return true;
+//         }
+
+//         return isSymmetricHelp(root->left,root->right);
+//     }
+// };
