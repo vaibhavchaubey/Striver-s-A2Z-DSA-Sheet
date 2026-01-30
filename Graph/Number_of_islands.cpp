e3300a563351fc2c4c89df880601571cc5553407
Vaibhav <vaibhavchaubey786@gmail.com>
2026-01-30 20:05:50 +0530

    Topic: Graphs

--- Graph/Number_of_islands.cpp
+++ Graph/Number_of_islands.cpp
@@ -99,7 +99,7 @@ Note: An island is either surrounded by water or the boundary of a grid and is f
 
 //         for(int i = 0; i < n; i++){
 //             for(int j = 0; j < m; j++){
-//                 // Start a new DFS when an unvisited land cell is found
+//                 // Start a new BFS when an unvisited land cell is found
 //                if(grid[i][j] == 'L' && !vis[i][j]){
 //                    islands++;
 //                    bfs(i, j, grid, vis);
@@ -107,6 +107,125 @@ Note: An island is either surrounded by water or the boundary of a grid and is f
 //             }
 //         }
 
+//         return islands;
+//     }
+// };
+
+
+
+
+
+/* Leetcode  Submission    (200. Number of Islands) */
+
+/* Problem Statement: Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water. */
+
+
+
+/* Solution 1: Using DFS */
+// Time Complexity: O(N * M)
+// Space Complexity: O(N * M)       // vis[]
+
+
+// class Solution {
+// public:
+//     bool isSafe(int r, int c, vector<vector<char>>& grid, vector<vector<bool>>& vis){
+//         int n = grid.size(), m = grid[0].size();
+
+//         return (r >= 0 && r < n && c >= 0 && c < m && grid[r][c] == '1' && !vis[r][c]);
+//     }
+//     void dfs(int row, int col, vector<vector<char>>& grid, vector<vector<bool>>& vis){
+//         vis[row][col] = true;
+
+//         // All 4 possible directions (vertical, horizontal)
+//         vector<int> dirRow = {-1, 1, 0, 0};
+//         vector<int> dirCol = {0, 0, -1, 1};
+
+//         for(int i = 0; i < 4; i++){
+//             int r = row + dirRow[i];
+//             int c = col + dirCol[i];
+
+//             if(isSafe(r, c, grid, vis)){
+//                 dfs(r, c, grid, vis);
+//             }
+//         }
+//     }
+//     int numIslands(vector<vector<char>>& grid) {
+//         // We can treat the matrix grid as a grid. Each Island is a
+//         // connected component. The task is to find no. of disconnected components in the graph.
+//         int m = grid.size(), n = grid[0].size();
+//         vector<vector<bool>> vis(m, vector<bool>(n, false));
+//         int islands = 0;
+
+//         for(int i = 0; i < m; i++){
+//             for(int j = 0; j < n; j++){
+//                 // Start a new DFS when an unvisited land cell is found
+//                 if(grid[i][j] == '1' && !vis[i][j]){
+//                     islands++;
+//                     dfs(i, j, grid, vis);
+//                 }
+//             }
+//         }
+        
+//         return islands;
+//     }
+// };
+
+
+/* Solution 2: Using BFS */
+// Time Complexity: O(N * M)
+// Space Complexity: O(N * M)       // vis[], queue
+
+
+// class Solution {
+// public:
+//     bool isSafe(int r, int c, vector<vector<char>>& grid, vector<vector<bool>>& vis){
+//         int n = grid.size(), m = grid[0].size();
+
+//         return (r >= 0 && r < n && c >= 0 && c < m && grid[r][c] == '1' && !vis[r][c]);
+//     }
+//     void bfs(int row, int col, vector<vector<char>>& grid, vector<vector<bool>>& vis){
+//         // All 4 possible directions (vertical, horizontal)
+//         vector<int> dirRow = {-1, 1, 0, 0};
+//         vector<int> dirCol = {0, 0, -1, 1};
+
+//         queue<pair<int, int>> q;
+//         q.push({row, col});
+//         vis[row][col] = true;
+
+//         while(!q.empty()){
+//             auto it = q.front();
+//             q.pop();
+
+//             for(int i = 0; i < 4; i++){
+//                 int r = it.first + dirRow[i];
+//                 int c = it.second + dirCol[i];
+
+//                 if(isSafe(r, c, grid, vis)){
+//                     vis[r][c] = true;
+//                     q.push({r, c});
+//                 }
+//             }
+//         }
+//     }
+//     int numIslands(vector<vector<char>>& grid) {
+//         // We can treat the matrix grid as a grid. Each Island is a
+//         // connected component. The task is to find no. of disconnected components in the graph.
+//         int m = grid.size(), n = grid[0].size();
+//         vector<vector<bool>> vis(m, vector<bool>(n, false));
+//         int islands = 0;
+
+//         for(int i = 0; i < m; i++){
+//             for(int j = 0; j < n; j++){
+//                 // Start a new DFS when an unvisited land cell is found
+//                 if(grid[i][j] == '1' && !vis[i][j]){
+//                     islands++;
+//                     bfs(i, j, grid, vis);
+//                 }
+//             }
+//         }
+        
 //         return islands;
 //     }
 // };
