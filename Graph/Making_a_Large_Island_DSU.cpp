fd3946e1c4c368baffee3a55770a34f62fa06f7b
Vaibhav <vaibhavchaubey786@gmail.com>
2026-04-26 18:22:44 +0530

    Topic: Graphs

--- Graph/Making_a_Large_Island_DSU.cpp
+++ Graph/Making_a_Large_Island_DSU.cpp
@@ -17,136 +17,137 @@ Whenever Graph is building dynamically or dynamic connections are happening, we 
 // Space Complexity: O(N * N)        // DisjointSet (parent[] and size[])
 
 
-class DisjointSet{
-    public:
-    vector<int> parent, size;
-    DisjointSet(int n){
-        parent.resize(n + 1);
-        size.resize(n + 1);
-        for(int i = 0; i <= n; i++){
-            parent[i] = i;                          // Initially, each node is its own parent (representative of its own set)
-            size[i] = 1;                           // Size is initialized to 1 for all nodes (each node is a set of size 1)
-        }
-    }
+// class DisjointSet{
+//     public:
+//     vector<int> parent, size;
+//     DisjointSet(int n){
+//         parent.resize(n + 1);
+//         size.resize(n + 1);
+//         for(int i = 0; i <= n; i++){
+//             parent[i] = i;                          // Initially, each node is its own parent (representative of its own set)
+//             size[i] = 1;                           // Size is initialized to 1 for all nodes (each node is a set of size 1)
+//         }
+//     }
 
-    int findUParent(int node){
-        if(node == parent[node]){
-            return node;
-        }
+//     int findUParent(int node){
+//         if(node == parent[node]){
+//             return node;
+//         }
 
-        return parent[node] = findUParent(parent[node]);            // Path Compression
-    }
+//         return parent[node] = findUParent(parent[node]);            // Path Compression
+//     }
 
-    void unionBySize(int u, int v){
-        int parent_u = findUParent(u); 
-        int parent_v = findUParent(v);
+//     void unionBySize(int u, int v){
+//         int parent_u = findUParent(u); 
+//         int parent_v = findUParent(v);
 
-        if(parent_u == parent_v){
-            return;
-        }
+//         if(parent_u == parent_v){
+//             return;
+//         }
 
-        if(size[parent_u] < size[parent_v]){
-            parent[parent_u] = parent_v;
-            size[parent_v] += size[parent_u];
-        }
-        else{                                                    // size[parent_v] < size[parent_u]
-            parent[parent_v] = parent_u;
-            size[parent_u] += size[parent_v];
-        }
-    }
+//         if(size[parent_u] < size[parent_v]){
+//             parent[parent_u] = parent_v;
+//             size[parent_v] += size[parent_u];
+//         }
+//         else{                                                    // size[parent_v] < size[parent_u]
+//             parent[parent_v] = parent_u;
+//             size[parent_u] += size[parent_v];
+//         }
+//     }
     
-    bool isConnected(int u, int v){
-        return findUParent(u) == findUParent(v);
-    }
-};
-class Solution {
-private:
-    // All 4 possible directions (vertical, horizontal)
-    vector<int> dirRow = {-1, 1, 0, 0};
-    vector<int> dirCol = {0, 0, -1, 1};
+//     bool isConnected(int u, int v){
+//         return findUParent(u) == findUParent(v);
+//     }
+// };
+// class Solution {
+// private:
+//     // All 4 possible directions (vertical, horizontal)
+//     vector<int> dirRow = {-1, 1, 0, 0};
+//     vector<int> dirCol = {0, 0, -1, 1};
 
-    bool isSafe(int r, int c, vector<vector<int>>& grid){
-        int n = grid.size(), m = grid[0].size();
+//     bool isSafe(int r, int c, vector<vector<int>>& grid){
+//         int n = grid.size(), m = grid[0].size();
 
-        return (r >= 0 && r < n && c >= 0 && c < m);
-    }
+//         return (r >= 0 && r < n && c >= 0 && c < m);
+//     }
 
-    // Function to add initial islands to the disjoint set data structure
-    void addInitialIslands(vector<vector<int>> &grid, DisjointSet &ds, int n) {
-        for(int row = 0; row < n; row++){
-            for(int col = 0; col < n; col++){
+//     // Function to add initial islands to the disjoint set data structure
+//     void addInitialIslands(vector<vector<int>> &grid, DisjointSet &ds, int n) {
+//         for(int row = 0; row < n; row++){
+//             for(int col = 0; col < n; col++){
 
-                // If the cell is not land, skip
-                if (grid[row][col] == 0){
-                    continue;
-                }
+//                 // If the cell is not land, skip
+//                 if (grid[row][col] == 0){
+//                     continue;
+//                 }
 
-                for(int i = 0; i < 4; i++){
-                    int r = row + dirRow[i];
-                    int c = col + dirCol[i];
+//                 for(int i = 0; i < 4; i++){
+//                     int r = row + dirRow[i];
+//                     int c = col + dirCol[i];
                     
-                    if(isSafe(r, c, grid)){
-                        if(grid[r][c] == 1){
-                            int u = (row * n) + col;        // nodeNo 
-                            int v = (r * n) + c;            // adjNodeNo 
-                            ds.unionBySize(u, v);      // O(α(N * N))
-                        }
-                    }
-                }
+//                     if(isSafe(r, c, grid)){
+//                         if(grid[r][c] == 1){
+//                             int u = (row * n) + col;        // nodeNo 
+//                             int v = (r * n) + c;            // adjNodeNo 
+//                             ds.unionBySize(u, v);      // O(α(N * N))
+//                         }
+//                     }
+//                 }
                 
-            }
-        }
-    }
-public:
-    int largestIsland(vector<vector<int>>& grid) {      
-        int n = grid.size();  
-        DisjointSet ds(n * n);
+//             }
+//         }
+//     }
+// public:
+//     int largestIsland(vector<vector<int>>& grid) {      
+//         int n = grid.size();  
+//         DisjointSet ds(n * n);
         
-        // Function call to add initial islands in the disjoint set
-        addInitialIslands(grid, ds, n);
+//         // Function call to add initial islands in the disjoint set
+//         addInitialIslands(grid, ds, n);
 
-        int ans = 0;
-        for(int row = 0; row < n; row++){
-            for(int col = 0; col < n; col++){
-                // If the cell is a land cell, skip
-                if (grid[row][col] == 1){
-                    continue;
-                }
+//         int ans = 0;
+//         for(int row = 0; row < n; row++){
+//             for(int col = 0; col < n; col++){
+//                 // If the cell is a land cell, skip
+//                 if (grid[row][col] == 1){
+//                     continue;
+//                 }
 
-                // Set to store the ultimate parents of neighboring islands (To avoid counting the same island multiple times if it is connected to the current cell from multiple sides)
-                set<int> components;
+//                 // Set to store the ultimate parents of neighboring islands (To avoid counting the same island multiple times if it is connected to the current cell from multiple sides)
+//                 set<int> components;
 
-                for(int i = 0; i < 4; i++){
-                    int r = row + dirRow[i];
-                    int c = col + dirCol[i];
+//                 for(int i = 0; i < 4; i++){
+//                     int r = row + dirRow[i];
+//                     int c = col + dirCol[i];
                     
-                    if(isSafe(r, c, grid)){
-                        if(grid[r][c] == 1){
-                            int v = (r * n) + c;            // adjNodeNo 
-                            int parent_v =  ds.findUParent(v);
-                            components.insert(parent_v);
-                        }
-                    }
-                }
+//                     if(isSafe(r, c, grid)){
+//                         if(grid[r][c] == 1){
+//                             int v = (r * n) + c;            // adjNodeNo 
+//                             int parent_v =  ds.findUParent(v);
+//                             components.insert(parent_v);
+//                         }
+//                     }
+//                 }
 
-                int size = 0;
+//                 int size = 0;
 
-                // Iterate on all the neighborinh ultimate parents
-                for(auto it : components){
-                    size += ds.size[it];
-                }
+//                 // Iterate on all the neighborinh ultimate parents
+//                 for(auto it : components){
+//                     size += ds.size[it];
+//                 }
 
-                // Store the maximum size of island
-                ans = max(ans, size + 1);
-            }
-        }
+//                 // Store the maximum size of island
+//                 ans = max(ans, size + 1);
+//             }
+//         }
 
-        // Edge case (when there is no 0 in the grid, i.e., the whole grid is one big island)
-        for (int cellNo = 0; cellNo < n * n; cellNo++) {
-            int parent_cell = ds.findUParent(cellNo);
-            ans = max(ans, ds.size[parent_cell]);
-        }
+//         // Edge case (when there is no 0 in the grid, i.e., the whole grid is one big island)
+//         for (int cellNo = 0; cellNo < n * n; cellNo++) {
+//             int parent_cell = ds.findUParent(cellNo);
+//             ans = max(ans, ds.size[parent_cell]);
+//         }
         
-        return ans;
-    }
-};
+//         return ans;
+//     }
+// };
+
